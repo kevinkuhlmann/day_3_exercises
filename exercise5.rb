7b442e2bbf305fbbb7f0727a6df115cc48932282
Kevin Kuhlmann <kevinkuhlmann0526@gmail.com>
2015-06-10 23:35:13 -0400

    redid code to make it simpler

--- exercise5.rb
+++ exercise5.rb
@@ -1,9 +1,8 @@
-def tempconvert(temp)
-  c = (temp - 32) * (5 / 9)
-  return c
+
+def convert(tf)
+  c = (tf - 32) * (5.0 / 9)
 end
 
-puts "What is your temperature in Farenheit?"
-  temp = gets.to_i
-
-puts "Your temperature in Celicus is #{tempconvert(temp)}"
+puts "whats your temperature"
+tf = gets.chomp.to_i
+puts "you temperature is #{convert(tf)}"
