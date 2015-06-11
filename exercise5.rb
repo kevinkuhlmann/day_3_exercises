b5e1c247ebad37f7049896a6a2541cec7710bc07
Kevin Kuhlmann <kevinkuhlmann0526@gmail.com>
2015-06-11 00:48:39 -0400

    limited decimal points on exercise5 and start part 1 of exercise6

--- exercise5.rb
+++ exercise5.rb
@@ -1,6 +1,7 @@
 
 def convert(tf)
   c = (tf - 32) * (5.0 / 9)
+  c.round(2)
 end
 
 puts "whats your temperature"
---
+++ exercise6.rb
@@ -0,0 +1,7 @@
+grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
+
+puts "1."
+
+grocery_list.each do |grocery|
+  puts "#{"." + grocery}"
+end
